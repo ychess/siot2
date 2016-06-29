de05a7891541fed32870ec7a013fad4653345f68
gugia <gugia@qq.com>
2016-06-29 17:09:50 +0800

    to be continued..

---
+++ src/main/java/com/xkt/siot/mina/protocol/MobileProtocolHead.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright 2016 XKT Ltd., Co.
+ * Licensed under the Apache License 2.0.
+ */
+package com.xkt.siot.mina.protocol;
+
+/**
+ * 移动端通讯约定类型
+ *
+ * @author L.X <gugia@qq.com>
+ */
+public class MobileProtocolHead {
+
+}
